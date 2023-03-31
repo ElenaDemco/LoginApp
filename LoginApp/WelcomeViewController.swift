e5ec288e5fa12638268f5851185fb53e883c0ab4
Elena <elena.demco@gmail.com>
2023-03-31 19:06:35 +0200

    Create new class for Welcome page

---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp
+//
+//  Created by Yelena Demchenko on 31.03.2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
