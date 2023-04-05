340ab036453f8a1879d1a3f054afb99e0f66cd3d
Elena <elena.demco@gmail.com>
2023-04-05 23:01:03 +0200

    Create Profile VC

---
+++ LoginApp/ViewControllers/ProfilePersonViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfilePersonViewController.swift
+//  LoginApp
+//
+//  Created by Yelena Demchenko on 05.04.2023.
+//
+
+import UIKit
+
+final class ProfilePersonViewController: UIViewController {
+    
+}
+
+func viewDidLoad() {
+    super.viewDidLoad()
+}
